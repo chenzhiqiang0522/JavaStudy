be7238e196ed71e2a8bea4f60c16410ce8125349
NickWiber <1249323612@qq.com>
2023-02-07 14:49:47 +0800

    增加for循环的方式

--- IdeaWorkspace/javaCode/2023.2.7流程控制/src/ClassPractice/WhilePrac1.java
+++ IdeaWorkspace/javaCode/2023.2.7流程控制/src/ClassPractice/WhilePrac1.java
@@ -5,11 +5,18 @@ package ClassPractice;
 
 public class WhilePrac1 {
     public static void main(String[] args) {
-        int i =1;
-        while(i<11){            //循环
-            if (i%2==0)         //  判断是否是偶数
+        int i = 1;
+        while (i < 11) {            //循环
+            if (i % 2 == 0)         //  判断是否是偶数
                 System.out.println(i);
             i++;
+        }
+        System.out.println("----------分割线-----------");
+
+        for (int b = 1; b < 11; b++){
+            if (b % 2 == 0) {
+                System.out.println(b);
+            }
         }
     }
 }
