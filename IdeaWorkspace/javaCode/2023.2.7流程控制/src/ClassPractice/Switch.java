babe313cba196324d1ff7bdd597e0fd8d4972b34
NickWiber <1249323612@qq.com>
2023-02-07 11:47:46 +0800

    Switch课堂练习代码

---
+++ IdeaWorkspace/javaCode/2023.2.7流程控制/src/ClassPractice/Switch.java
@@ -0,0 +1,20 @@
+package ClassPractice;
+
+public class Switch {
+    public static void main(String[] args) {
+        int a = 8;
+        switch (a) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("今天也好好好工作鸭!!!!");
+                break;
+            default:
+                System.out.println("输入有误");
+        }
+    }
+}
