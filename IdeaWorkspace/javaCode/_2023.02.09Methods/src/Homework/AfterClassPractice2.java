fdaa6e9a5bf59c62e2df73d50706016dd60ca8c7
NickWiber <1249323612@qq.com>
2023-02-09 16:56:57 +0800

    课后第二题代码

--- IdeaWorkspace/javaCode/_2023.02.09Methods/src/Homework/AfterClassPractice2.java
+++ IdeaWorkspace/javaCode/_2023.02.09Methods/src/Homework/AfterClassPractice2.java
@@ -9,11 +9,20 @@ package Homework;
  * Filename：AfterClassPractice2
  */
 
+import java.util.Arrays;
+
 /**
  * 定义一个方法，接受三个int参数，返回最小的一个数。
  */
 public class AfterClassPractice2 {
     public static void main(String[] args) {
+        int[] arr = new int[]{1,5,9,6,5,2,4,3,6,8,2};
+        min(arr);
+    }
 
+    public static int[] min(int[] arr){
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        return arr;
     }
 }
